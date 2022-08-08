82b0dc706666c9bdd53ade7d972672517f4de77d
banditsbeware <david.rademacher@mavs.uta.edu>
2022-08-07 20:25:43 -0500

    baz

--- baz.php
+++ baz.php
@@ -21,18 +21,36 @@
     
     <h3>baz - laptop</h3>
 
+    <img src='static/media/macback.png' />
+
     <p>
       2021 16" M1 MacBook Pro, purchased in my last semester of college in spring of 2022; Used often for reading and coding remotely. Wallpaper: <a href='https://stsci-opo.org/STScI-01G8GX1KMWX2XA4PK2EWM7KE3R.png'>full resolution NGC 3324 image</a> (125MB) from the JWST debut
     </p>
 
     <img src='static/media/MacOS_rice.png' />
-    <!-- include photo of back of laptop lid -->
 
     <h3>software</h3>
     <ul>
+      <li>Package manager: Homebrew</li>
       <li>Terminal: <a>Iterm2</a></li>
-      <li>Editor: <a>NeoVim</a></li>
-
+      <li>Editor: <a>NeoVim</a> or Visual Studio Code</li>
+      <li>Browser: Firefox, with plugins<ul style='padding-left: 20px'>
+        <li>Vimium</li>
+        <li>uBlock Origin</li>
+        <li>Dark Reader</li>
+        <li>Sidebery</li>
+        <li>Download All Images</li>
+        <li>I Don't Care About Cookies</li>
+      </ul></li>
+      <li>RSS aggregator: Reeder</li>
+      <li>PDF reader: Skim</li>
+      <li>Miscellaneous<ul style='padding-left: 20px'>
+        <li>Discord</li>
+        <li>GIMP</li>
+        <li>Hex Fiend</li>
+        <li>Notion</li>
+        <li>Spotify</li>
+      </ul></li>
     </ul>
 
     <p id='cp'>&#169; david rademacher <?php echo date("Y"); ?></p> 
