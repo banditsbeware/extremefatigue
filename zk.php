78f4f846a72fc2022f2567210ed0c140d883c637
banditsbeware <david.rademacher@mavs.uta.edu>
2022-08-10 14:29:49 -0500

    blog n zk

---
+++ zk.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" contents="">
+    <link rel="stylesheet" href="static/style.css" type="text/css">
+    <title>david's k&#333;ans</title>
+    <style>
+      p.koan {
+        width: 200%;
+        line-height: 1.5;
+        margin-left: -50%;
+        margin-top: 100px;
+      }
+    </style>
+  </head>
+
+  <body>
+    
+    <?php include "static/nav.php"; ?>
+
+    <h3>zk - Zen k&#333;ans</h3>
+
+    <p class='koan'>
+      Before I began my studies in Zen, I thought a tree was a tree and a stone was a stone.</br>
+      When I started to study Zen, I could see that a tree was not a tree and a stone was not a stone.</br>
+      Now that I am a Zen master, I know that a tree is a tree, and a stone is a stone.&nbsp;
+      <small><a target='_blank' href='https://news.ycombinator.com/item?id=32398391#32399093'>[1]</a></small>
+    </p>
+
+    <p class='koan'>
+Hogen of Seiryo monastery was about to lecture before dinner when he noticed that the bamboo screen, lowered for meditation, had not been rolled up. He pointed to it. Two monks arose wordlessly from the audience and rolled it up. Hogen, observing the physical moment, said, "The state of the first monk is good, not that of the second."
+      <small><a>[GEB]</a></small>
+    </p>
+
+    <p class='koan'>
+The student Doko came to a Zen master, and said: "I am seeking the truth. In what state of mind should I train myself, so as to find it?"</br>
+Said the master, "There is no mind, so you cannot put it in any state. There is no truth, so you cannot train yourself for it."</br>
+"If there is no mind to train, and no truth to find, why do you have these monks gather before you every day to study Zen and train themselves for this study?"</br>
+"But I haven't an inch of room here," said the master, "so how could the monks gather? I have no tongue, so how could I call them together or teach them?"</br>
+"Oh, how can you lie like this?" asked Doko.</br>
+"But if I have no tongue to talk to others, how can I lie to you?" asked the master.</br>
+Then Doko said sadly, "I cannot follow you. I cannot understand you."</br>
+"I cannot understand myself," said the master.
+    <small><a>[GEB]</a></small>
+    </p>
+
+    <p class='koan'> 
+Hyakujo wished to send a monk to open a new monastery. He told his pupils that whoever answered a question most ably would be appointed.</br>
+Placing a water vase on the ground, he asked: "Who can say what this is without calling its name?"</br>
+The chief monk said: "No one can call it a wooden shoe."</br>
+Isan, the cooking monk, tipped over the vase with his foot and went out.</br>
+Hyakujo smiled and said: "The chief monk loses."</br>
+And Isan became the master of the new monastery.
+    <small><a>[GEB]</a></small>
+    </p>
+
+
+  </body>
+
+</html>
