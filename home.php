180404ad511eccb345f32c2f812940cf3e080328
banditsbeware <david.rademacher@mavs.uta.edu>
2022-08-31 12:37:28 -0500

    anonymized

--- home.php
+++ home.php
@@ -4,27 +4,27 @@
     <meta charset="UTF-8">
     <meta name="description" contents="">
     <link rel="stylesheet" href="static/style.css" type="text/css">
-    <title>david's website</title>
+    <title>. . .</title>
   </head>
 
   <body>
     
     <?php include "static/nav.php"; ?>
 
-    <h3>home - david's website</h3>
+    <h3>home - extreme fatigue</h3>
 
-    <ul>
+<!--<ul>
       <li>23</li>
       <li>Fort Worth, TX</li>
       <li>running, coffee, music, anime, vintage t-shirts</li>
       <li>degrees in computer science &amp; physics</li>
       <li>DCI (2015-19)</li>
       <li>software engineer, flight control computers, Lockheed Martin aeronautics</li>
-    </ul>
+    </ul>-->
 
-    <p id='cp'>&#169; david rademacher <?php echo date("Y"); ?></p> 
+    <p id='cp'>&#169; extreme fatigue <?php echo date("Y"); ?></p> 
 
-    <img style='width:300px;margin-top:0px;' src='static/media/me.jpg' />
+<!--<img style='width:300px;margin-top:0px;' src='static/media/me.jpg' />-->
 
   </body>
 
