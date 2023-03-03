91db53c4b2222f8454291d4c54018c5fd62fb5a3
banditsbeware <david.rademacher@mavs.uta.edu>
2023-03-02 19:38:42 -0600

    some layout fixes; main banner

--- home.php
+++ home.php
@@ -5,27 +5,23 @@
     <meta name="description" contents="">
     <link rel="stylesheet" href="static/style.css" type="text/css">
     <link rel="stylesheet" href="static/home.css" type="text/css">
-    <title>. . .</title>
+    <title>%</title>
   </head>
   <body>
     <?php include "static/nav.php"; ?>
     <main>
-        <div id='announcement'>yes the layout is very bad i am working on that (sometimes)</div>
-        <div id='thoughts'>
-          <?php
-            $thoughts = file("static/thoughts.txt");
-            echo "<p>".$thoughts[array_rand($thoughts)]."</p>";      
-          ?>
-          <img src='static/media/dave1.png'/>
+        
+        <div id='main-header' class='maincolumn'>
+            <pre><?php include "static/banner.txt"; ?></pre>
         </div>
 
-        <div id='declarations'>
+        <div id='declarations' class='maincolumn'>
             <?php
                 $init = ["int", "float", "double", "long", "char", "bool"];
                 $decl = file("static/variables.txt");
 
                 echo "<code> // DECLARATIONS</br>";
-                for ($i=0; $i<random_int(1,3); $i++)
+                for ($i=0; $i<random_int(2,5); $i++)
                     echo "<span style='color:blue'><em>"
                         .$init[array_rand($init)]
                         ."</em></span> "
@@ -35,7 +31,7 @@
             ?>
         </div>
 
-        <div id='studytopics'>
+        <div id='studytopics' class='maincolumn'>
           <h2>Interesting topics</h2>
           <ul><?php
             $topics = file("static/studytopics.txt");
@@ -45,25 +41,7 @@
           ?></ul>
         </div>
 
-    <!--<div id='you'>
-          <p>Does this sound like <em>you?</em></p>
-          <ul>
-            <li>you have a great need for other people to like &amp; admire you</li>
-            <li>you have a tendency to be critical of yourself</li>
-            <li>you have lots of unused capacity which you haven't used to your advantage</li>
-            <li>you may have some personality weaknesses but can compensate for them</li>
-            <li>while disciplined &amp; self controlled outside, you tend to be worrisome &amp; insecure inside</li>
-            <li>you prefer some change &amp; variety, are dissatisfied with restrictions/limitations</li>
-            <li>you are an independent thinker &amp; need satisfactory proof of claims by others</li>
-            <li>you find it unwise to be too quick in revealing yourself to others</li>
-            <li>you are sometimes extraverted, but sometimes introverted</li>
-            <li>some of your aspirations tend to be pretty unrealistic</li>
-            <li>security is one of your major goals in life</li>
-          </ul>
-          <p><small>if so, you may be susceptible to pseudoscientific grifts; please see <a target='_blank' href='https://en.wikipedia.org/wiki/Barnum_effect'>Barnum effect</a></small></p>
-        </div>-->
-
-        <div id='updates'>
+        <div id='updates' class='maincolumn'>
           <h4>Update history</h4>
           <div style='height: 200px; overflow-y: scroll;'>
           <?php
@@ -72,7 +50,17 @@
           ?>
         </div></div>
 
-        <div id='buttons'>
+        <div id='barnum' class='maincolumn'> <?php include "static/barnum.html"; ?> </div>
+
+        <div id='thoughts' class='maincolumn'>
+          <?php
+            $thoughts = file("static/thoughts.txt");
+            echo "<p>".$thoughts[array_rand($thoughts)]."</p>";      
+          ?>
+          <img src='static/media/dave1.png'/>
+        </div>
+
+        <div id='buttons' class='maincolumn'>
           <?php
             $buttons = [
               ["88x31.gif", "https://cyber.dabamos.de/88x31/index.html"], 
@@ -109,14 +97,12 @@
     <p id='cp'>&#169; extreme fatigue <?php echo date("Y"); ?></p> 
   </body>
 
-<!--
-  <?php include "anim/_base.html"; ?>
+  <?php // include "anim/_base.html"; ?>
   
   <?php
-    $anims = glob("anim/anim*.html");
-    $file = $anims[array_rand($anims, 1)];
+//  $anims = glob("anim/anim*.html");
+//  $file = $anims[array_rand($anims, 1)];
 //  $file = "anim/anim9.html";
-    include $file;    
+//  include $file;    
   ?>
--->
 </html>
