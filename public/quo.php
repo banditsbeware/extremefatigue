f823d5650ca188099ae8cd2cc382d6177ed7ab35
banditsbeware <david.rademacher@mavs.uta.edu>
2025-03-30 11:44:53 -0500

    revival

--- public/quo.php
+++ public/quo.php
@@ -49,7 +49,9 @@
 <div id="quotes"><?php
 
 $k = array(
-  [
+  [ "He who loves his body more than dominion over the empire can be given custody over the empire.",
+    "Lao Tzu, <em>Tao Te Ching</em>"
+  ],[
     "I grow little of the food I eat, and of the little I do grow I did not breed or perfect the seeds. I do not make any of my own clothing. I speak a language I did not invent or refine. I did not discover the mathematics I use. I am protected by freedoms and laws I did not concieve of or legislate, and do not enforce or adjudicate. I am moved by music I did not create myself. When I needed medical attention, I was helpless to help myself survive. I did not invent the transistor, the microprocessor, object-oriented programming, or most of the technology I work with. I love and admire my species, living and dead, and am totally dependent on them for my life and well being.",
     "Steve Jobs"
   ],[
